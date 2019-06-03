8047891753e4ebcebe9a8b6d2bc583d0e3e3a95b
Pradnyesh <pradnyesh.patil@sjsu.edu>
2019-06-03 16:14:49 -0700

    basic lambda on collection

--- Java Practice/src/CollectionIteration.java
+++ Java Practice/src/CollectionIteration.java
@@ -3,12 +3,23 @@ import java.util.List;
 
 public class CollectionIteration {
 
-    List<Person> people = Arrays.asList(
-            new Person("john", "doe",24),
-            new Person("abe", "smith",24),
-            new Person("ross", "gellar",24),
-            new Person("john", "snow",24),
-            new Person("joey", "tribiani",24)
-    );
+    public static void main(String[] args) {
+
+        List<Person> people = Arrays.asList(
+                new Person("john", "doe",24),
+                new Person("abe", "smith",24),
+                new Person("ross", "gellar",24),
+                new Person("john", "snow",24),
+                new Person("joey", "tribiani",24)
+        );
+
+        System.out.println("Using java 8 lambdas internal loop");
+
+        people.forEach(System.out::println);
+
+        people.forEach(p->System.out.println(p.getFname()));
+
+
+    }
 
 }
